bbd17f199490c178f59f5e21d79e8c1695362f11
James <april9288@gmail.com>
2019-08-25 15:57:39 -0700

    feat: add brotli compression plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,25 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const BrotliPlugin = require("brotli-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
+const merge = require('webpack-merge');
+const BrotliPlugin = require('brotli-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
-  plugins: [
-    new CompressionPlugin({
-      filename: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.(js|css|html|svg)$/,
-      threshold: 8192,
-      minRatio: 0.8
-    }),
-    new BrotliPlugin({
-      asset: "[path].br[query]",
-      test: /\.(js|css|html|svg)$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  ]
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new CompressionPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 8192,
+            minRatio: 0.8
+        }),
+        new BrotliPlugin({
+            asset: '[path].br[query]',
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
 });
